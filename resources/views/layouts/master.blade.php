bbc12bc17bbe8b0e8a634c8fdfce306910437ace
NOUAOURI Abdelkarim <57526755+abdellmum@users.noreply.github.com>
2021-07-07 09:58:50 +0100

    views update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,22 +37,7 @@
   <body>
 
 <div class="container">
-  <header class="blog-header py-3">
-    <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-8 pt-1">
-        {{-- <a class="link-secondary" href="#">Subscribe</a> --}}
-      </div>
-      {{-- <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="#">"logo"</a>
-      </div> --}}
-      <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="link-secondary" href="#" aria-label="Search">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-        </a>
-        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-      </div>
-    </div>
-  </header>
+
 
   <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between">
@@ -65,12 +50,15 @@
       <a class="p-2 link-secondary" href="#">Accessoires</a>
       <a class="p-2 link-secondary" href="#">Setup</a>
       <a class="p-2 link-secondary" href="#">Contact</a>
-      <a class="p-2 link-secondary" href="#">Panier</a>
+      <a class="p-2 link-secondary" href="#">login</a>
+      <a class="link-secondary quantité" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </svg> <span class="badge badge-pill badge-dark "></span> {{ Cart::count()}}</a>
 
     </nav>
   </div>
   @if(session('success'))
-      <div class="alert alert_sucess">
+      <div class="alert alert-success">
           {{ session('success') }}
       </div>
 
@@ -80,9 +68,9 @@
 <main class="container">
   <div class="p-4 p-md-5 mb-4 text-white header__hero rounded ">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
+      <h1 class="display-4 fst-italic">yougamers</h1>
+      <p class="lead my-3">La plus grande boutique des gamers en ligne au maroc .avec des meilleurs prix a jamais vu</p>
+      <p class="lead mb-0"><a href="#" class="text-white fw-bold">Voir plus...</a></p>
     </div>
   </div>
 
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -36,40 +36,43 @@
   <body>
 
 <div class="container">
-  <header class="blog-header py-3">
-    <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-4 pt-1">
-        <a class="link-secondary" href="#">Subscribe</a>
-      </div>
-      <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="{{route('products.index')}}">Logo</a>
-      </div>
-      <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="link-secondary" href="#" aria-label="Search">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-        </a>
-        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-      </div>
-    </div>
-  </header>
+    {{-- <header class="blog-header py-3"> --}}
+        {{-- <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-8 pt-1">
+            <a class="link-secondary quantité" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+    </svg> <span class="badge badge-pill badge-dark "></span> {{ Cart::count()}}</a>
+          </div> --}}
+          {{-- <div class="col-4 text-center">
+            <a class="blog-header-logo text-dark" href="#">"logo"</a>
+          </div> --}}
+          {{-- <div class="col-4 d-flex justify-content-end align-items-center">
+            <a class="link-secondary" href="#" aria-label="Search">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+            </a>
+            <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+          </div>
+        </div>
+      </header> --}}
 
-  <div class="nav-scroller py-1 mb-2">
-    <nav class="nav d-flex justify-content-between">
-      <a class="p-2 link-secondary" href="#">World</a>
-      <a class="p-2 link-secondary" href="#">U.S.</a>
-      <a class="p-2 link-secondary" href="#">Technology</a>
-      <a class="p-2 link-secondary" href="#">Design</a>
-      <a class="p-2 link-secondary" href="#">Culture</a>
-      <a class="p-2 link-secondary" href="#">Business</a>
-      <a class="p-2 link-secondary" href="#">Politics</a>
-      <a class="p-2 link-secondary" href="#">Opinion</a>
-      <a class="p-2 link-secondary" href="#">Science</a>
-      <a class="p-2 link-secondary" href="#">Health</a>
-      <a class="p-2 link-secondary" href="#">Style</a>
-      <a class="p-2 link-secondary" href="#">Travel</a>
-    </nav>
-  </div>
-</div>
+      <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex justify-content-between">
+
+          <a class="p-2 link-secondary active" href="#">Accueil</a>
+          <a class="p-2 link-secondary" href="#">Store</a>
+
+          <a class="p-2 link-secondary" href="#">Pc Gamer</a>
+          <a class="p-2 link-secondary" href="#">Console</a>
+          <a class="p-2 link-secondary" href="#">Accessoires</a>
+          <a class="p-2 link-secondary" href="#">Setup</a>
+          <a class="p-2 link-secondary" href="#">Contact</a>
+          <a class="p-2 link-secondary" href="#">login</a>
+          <a class="link-secondary quantité" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+          </svg> <span class="badge badge-pill badge-dark "></span> {{ Cart::count()}}</a>
+
+        </nav>
+      </div>
 
 <main class="container">
   <div class="p-4 p-md-5 mb-4 text-white rounded   header__hero">
@@ -91,11 +94,13 @@
             <strong class="d-inline-block mb-2 text-primary">{{ $product->getPrice()}}</strong>
             <form action="{{Route('cart.store')}}" method="POST">
             @csrf
-            <input type="hidden" name="id" value="{{ $product->id }}">
-            <input type="hidden" name="tittle" value="{{ $product->tittle}}">
-            <input type="hidden" name="price" value="{{ $product->price }}">
+            <input type="hidden" name="product_id" value="{{ $product->id }}">
+            {{-- <input type="hidden" name="tittle" value="{{ $product->tittle}}">
+            <input type="hidden" name="price" value="{{ $product->price }}"> --}}
 
-                <button type="submit" class="btn btn-info" >Ajouter au panier</button>
+                <button type="submit" class="btn btn-info addtocart" > <svg class="cart__icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                  </svg>Ajouter au panier</button>
             </form>
           </div>
           <div class="col-auto d-none d-lg-block">
