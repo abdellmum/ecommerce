98753fd1bee2ec631a596790ff8969cfb23fc440
NOUAOURI Abdelkarim <57526755+abdellmum@users.noreply.github.com>
2021-07-08 21:42:34 +0100

    Update index.blade.php

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+@if (Cart::count() > 0)
 <div class="px-4 px-lg-0">
     <!-- For demo purpose -->
     <div class="container text-white py-5 text-center">
@@ -40,15 +41,29 @@
                     <tr>
                         <th scope="row" class="border-0">
                           <div class="p-2">
-                            <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
+                            <img src="{{ $product->model->image }}" alt="" width="70" class="img-fluid rounded shadow-sm">
                             <div class="ml-3 d-inline-block align-middle">
-                              <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">Timex Unisex Originals</a></h5><span class="text-muted font-weight-normal font-italic d-block">Category: Watches</span>
+                              <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">{{ $product->model->tittle }}</a></h5><span class="text-muted font-weight-normal font-italic d-block">Category: Watches</span>
                             </div>
                           </div>
                         </th>
-                        <td class="border-0 align-middle"><strong>$79.00</strong></td>
-                        <td class="border-0 align-middle"><strong>3</strong></td>
-                        <td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+                        <td class="border-0 align-middle"><strong>{{ $product->model->getprice() }}</strong></td>
+                        <td class="border-0 align-middle"><strong>1</strong></td>
+                        <td class="border-0 align-middle">
+                            <form action="{{ route('Cart.destroy'),$product->rowId }}" method="post">
+                                @csrf
+                                @method('DELETE');
+
+
+                            <button type="submit" class="text-dark bg-warning"><svg class
+                             xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path  d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                          </svg>
+                        </button>
+                    </form>
+
+                        </td>
                       </tr>
 
                     @endforeach
@@ -98,6 +113,12 @@
       </div>
     </div>
   </div>
+@else
+<div class="alert alert-info" role="alert">
+    Votre panier est vide !
+  </div>
 
+
+@endif
 
 @endsection
