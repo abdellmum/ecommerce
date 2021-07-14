7dcd11d6445a43330e33dc57ef2165cb67a8ebc0
NOUAOURI Abdelkarim <57526755+abdellmum@users.noreply.github.com>
2021-07-14 09:52:29 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,10 +8,10 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
     <title>Youcommerce</title>
+    @yield('extra-script')
+
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/blog/">
-
-
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -32,7 +32,7 @@
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="/css/main.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 
   </head>
   <body>
@@ -254,7 +254,7 @@
   </p>
 </footer>
 
-
+@yield('extra-js')
 
   </body>
 </html>
