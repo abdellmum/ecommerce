a146eef857d67d49502e76e3a348d85a89346e93
NOUAOURI Abdelkarim <57526755+abdellmum@users.noreply.github.com>
2021-07-14 09:52:33 +0100

    Update show.blade.php

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -95,9 +95,7 @@
             <form action="{{Route('cart.store')}}" method="POST">
             @csrf
             <input type="hidden" name="product_id" value="{{ $product->id }}">
-            {{-- <input type="hidden" name="tittle" value="{{ $product->tittle}}">
-            <input type="hidden" name="price" value="{{ $product->price }}"> --}}
-
+            
                 <button type="submit" class="btn btn-info addtocart" > <svg class="cart__icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                   </svg>Ajouter au panier</button>
